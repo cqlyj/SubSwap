472c60031e57066a68e304482ff67d006248e924
cqlyj <luoyingjie0721@gmail.com>
2025-02-17 14:32:00 +0800

    feat: add WrappedSubscription contract for minting and burning ERC20 tokens

---
+++ src/WrappedSubscription.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract WrappedSubscription is ERC20, Ownable {
+    using Strings for uint256;
+
+    constructor(
+        uint256 _tokenID
+    )
+        ERC20(
+            string(
+                abi.encodePacked("Wrapped Subscription ", _tokenID.toString())
+            ),
+            string(abi.encodePacked("wSub_", _tokenID.toString()))
+        )
+        Ownable(msg.sender)
+    {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
