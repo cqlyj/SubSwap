3efc51250af01ded4745505d5d1582dba64317af
cqlyj <luoyingjie0721@gmail.com>
2025-02-25 23:02:33 +0800

    feat: enhance WrappedSubscription contract with plan ID management and token tracking

--- src/WrappedSubscription.sol
+++ src/WrappedSubscription.sol
@@ -8,31 +8,86 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 contract WrappedSubscription is ERC20, Ownable {
     using Strings for uint256;
 
-    uint256 private s_tokenID;
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 private s_planID;
+    mapping(address owner => uint256[] tokenIds) private s_ownerToTokenIds;
+    mapping(address owner => mapping(uint256 tokenId => uint256 amount))
+        private s_ownerToTokenIdToAmount;
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error WrappedSubscription__InsufficientBalance(
+        address owner,
+        uint256 tokenId
+    );
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
 
     constructor(
-        uint256 _tokenID
+        uint256 _planId
     )
         ERC20(
             string(
-                abi.encodePacked("Wrapped Subscription ", _tokenID.toString())
+                abi.encodePacked("Wrapped Subscription ", _planId.toString())
             ),
-            string(abi.encodePacked("wSub_", _tokenID.toString()))
+            string(abi.encodePacked("wSub_", _planId.toString()))
         )
         Ownable(msg.sender)
     {
-        s_tokenID = _tokenID;
+        s_planID = _planId;
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    /*//////////////////////////////////////////////////////////////
+                     EXTERNAL AND PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function mint(
+        address to,
+        uint256 amount,
+        uint256 tokenId
+    ) external onlyOwner {
+        s_ownerToTokenIds[to].push(tokenId);
+        s_ownerToTokenIdToAmount[to][tokenId] = amount;
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) external onlyOwner {
+    function burn(
+        address from,
+        uint256 amount,
+        uint256 tokenId
+    ) external onlyOwner {
+        if (s_ownerToTokenIdToAmount[from][tokenId] < amount) {
+            revert WrappedSubscription__InsufficientBalance(from, tokenId);
+        }
+        s_ownerToTokenIdToAmount[from][tokenId] -= amount;
         _burn(from, amount);
     }
 
-    function tokenId() external view returns (uint256) {
-        return s_tokenID;
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    function getPlanId() external view returns (uint256) {
+        return s_planID;
+    }
+
+    function getTokenIds(
+        address owner
+    ) external view returns (uint256[] memory) {
+        return s_ownerToTokenIds[owner];
+    }
+
+    function getTokenAmount(
+        address owner,
+        uint256 tokenId
+    ) external view returns (uint256) {
+        return s_ownerToTokenIdToAmount[owner][tokenId];
     }
 }
